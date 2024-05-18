7994525cd4888f93d8ce35cabdabf8eb6d0cf5dd
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-05-18 17:51:10 +0700

    feat: Render dummy views with basic navigation

--- app/src/main/java/uni/aeh/tasktracker/MainActivity.kt
+++ app/src/main/java/uni/aeh/tasktracker/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import uni.aeh.tasktracker.ui.theme.TaskTrackerTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import uni.aeh.tasktracker.core.ui.theme.TaskTrackerTheme
+import uni.aeh.tasktracker.details.ui.DetailsScreen
+import uni.aeh.tasktracker.home.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +24,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "details") {
+                        composable("home") { HomeScreen(navController,"dope name" ) }
+                        composable("details") { DetailsScreen(navController) }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TaskTrackerTheme {
-        Greeting("Android")
     }
 }
