9bfcbcda82154368a6ed43bf609f71953ac440d3
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-05-25 16:35:54 +0700

    feat: Add main application class and add notate activity

--- app/src/main/java/uni/aeh/tasktracker/MainActivity.kt
+++ app/src/main/java/uni/aeh/tasktracker/MainActivity.kt
@@ -6,17 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import uni.aeh.tasktracker.core.ui.theme.TaskTrackerTheme
 import uni.aeh.tasktracker.details.ui.DetailsScreen
 import uni.aeh.tasktracker.home.ui.HomeScreen
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/uni/aeh/tasktracker/TaskTrackerApplication.kt
@@ -0,0 +1,7 @@
+package uni.aeh.tasktracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TaskTrackerApplication : Application()
