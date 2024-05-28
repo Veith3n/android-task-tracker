108ec32c066df8e33890e31a4ed2e418484a9dab
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-05-28 11:04:28 +0700

    feat: Mark completed and overdued tasks

--- app/src/main/java/uni/aeh/tasktracker/core/ui/theme/task/TaskItem.kt
+++ app/src/main/java/uni/aeh/tasktracker/core/ui/theme/task/TaskItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import uni.aeh.tasktracker.core.data.model.Task
 
@@ -25,18 +26,33 @@ fun TaskItem(
         ) {
             Text(
                 text = task.title,
-                style = MaterialTheme.typography.headlineSmall
+                style = MaterialTheme.typography.headlineSmall,
+                color = resolveTextColor(task)
             )
+           
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = task.description,
-                style = MaterialTheme.typography.bodyLarge
+                style = MaterialTheme.typography.bodyLarge,
+                color = resolveTextColor(task)
             )
+
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = "Due Date: ${task.formattedDueDate}",
-                style = MaterialTheme.typography.bodyMedium
+                style = MaterialTheme.typography.bodyMedium,
+                color = resolveTextColor(task)
             )
         }
+    }
+
+}
+
+@Composable
+private fun resolveTextColor(task: Task): Color {
+    return when {
+        task.isOverdue -> MaterialTheme.colorScheme.error
+        task.isCompleted -> Color(0xFF388E3C)
+        else -> MaterialTheme.colorScheme.onSurface
     }
 }
